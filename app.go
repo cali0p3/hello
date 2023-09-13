5db5555548069fc96ae69baad365be969579deae
Roberta Longoni <robertta.longoni@gmail.com>
2023-09-13 08:57:34 -0300

    Add changes

--- app.go
+++ app.go
@@ -2,57 +2,54 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
+
 	exibeIntroducao()
 	exibeMenu()
 	comando := leComando()
 
-	/* if comando == 1 {
-		fmt.Println("Monitorando")
-	} else if comando == 2{
-		fmt.Println("Exibindo Logs")
-	} else if comando == 3 {
-		fmt.Println("Saindo do Programa")
-	} else {
-		fmt.Println("Não reconheço esse comando")
-	} */
-
 	switch comando {
 	case 1:
-		fmt.Println("Monitorando")
+		iniciarMonitoramento()
 	case 2:
-		fmt.Println("Exibindo Logs")
-	case 3:
-		fmt.Println("Saindo do Programa")
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
 		os.Exit(0)
 	default:
-		fmt.Println("Não reconheço esse comando")
+		fmt.Println("Não conheço este comando")
 		os.Exit(-1)
 	}
-
 }
+
 func exibeIntroducao() {
-	nome := "Caliope"
+	nome := "Douglas"
 	versao := 1.2
-	fmt.Println("Olá, sr(a).", nome)
-	fmt.Println("Este programa eta na versão:", versao)
-
-}
-
-func leComando() int {
-	var comandoLido int
-	fmt.Scanf("%d", &comandoLido) //o comando fmt.Scan(&comando) pode ser usado visto que o tipo de variavel já foi declarado anteriormente.
-
-	fmt.Println("O comando selecionado foi:", comandoLido)
-
-	return comandoLido
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
 }
 
 func exibeMenu() {
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
-	fmt.Println("3- Sair do Programa")
+	fmt.Println("0- Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://eldorado.org.br/"
+	resp, _ := http.Get(site)
+	fmt.Println(resp)
 }
