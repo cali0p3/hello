961a2a399f25e98c6272a628f53f756252f364ce
Roberta Longoni <robertta.longoni@gmail.com>
2023-09-15 01:15:54 -0300

    Improving Website Monitoring

--- app.go
+++ app.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -13,6 +16,7 @@ const delay = 5
 func main() {
 
 	exibeIntroducao()
+	lerSitesDoArquivo()
 	for {
 		exibeMenu()
 		comando := leComando()
@@ -57,9 +61,10 @@ func leComando() int {
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
 
-	sites := []string{"https://random-status-code.herokuapp.com/",
-		"https://www.alura.com.br", "https://www.caelum.com.br", "http://www.eldorado.org.br", "http://google.com.br"}
+	//sites := []string{"https://random-status-code.herokuapp.com/",
+	//"https://www.alura.com.br", "https://www.caelum.com.br", "http://www.eldorado.org.br", "http://google.com.br"}
 
+	sites := lerSitesDoArquivo()
 	for i := 0; i < monitoramentos; i++ {
 		for i, site := range sites {
 			fmt.Println("Testando site", i, ":", site)
@@ -74,11 +79,40 @@ func iniciarMonitoramento() {
 }
 
 func testaSite(site string) {
-	resp, _ := http.Get(site)
+	resp, err := http.Get(site)
+	if err != nil {
+		fmt.Println("Ocorreu um erro!")
+		return
+	}
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site", site, "foi carregado com sucesso!")
 	} else {
 		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
 	}
+}
+
+func lerSitesDoArquivo() []string {
+
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro", err)
+	
+	}
+
+	leitor := bufio.NewReader(arquivo)
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		sites = append(sites, linha)
+		if err == io.EOF {
+			break
+		}
+	}
+
+	arquivo.Close()
+	return sites
 }
