3b151fdd648096243b06011dc34ecc4a35c993f3
Roberta Longoni <robertta.longoni@gmail.com>
2023-09-15 01:47:13 -0300

    Saving the monitoring logs

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -16,7 +17,9 @@ const delay = 5
 func main() {
 
 	exibeIntroducao()
-	lerSitesDoArquivo()
+
+	registraLog("site-falso", false)
+
 	for {
 		exibeMenu()
 		comando := leComando()
@@ -87,8 +90,10 @@ func testaSite(site string) {
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site", site, "foi carregado com sucesso!")
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		registraLog(site, false)
 	}
 }
 
@@ -100,7 +105,7 @@ func lerSitesDoArquivo() []string {
 
 	if err != nil {
 		fmt.Println("Ocorreu um erro", err)
-	
+
 	}
 
 	leitor := bufio.NewReader(arquivo)
@@ -115,4 +120,16 @@ func lerSitesDoArquivo() []string {
 
 	arquivo.Close()
 	return sites
+}
+
+func registraLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + site + "- online: " + strconv.FormatBool(status) + "\n")
+			//https://go.dev/src/time/format.go
+			//https://pkg.go.dev/time
+	arquivo.Close()
 }
